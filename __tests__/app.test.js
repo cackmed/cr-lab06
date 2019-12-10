4769f19268f698f8d9d7910acdd467acea4798d7
Caleb Pendergraft <cackmed@gmail.com>
2019-12-10 13:40:00 -0800

    most of the tests are passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,51 +3,47 @@ const app = require('../lib/app');
 
 describe('when you hit a route it responses in the right way', () => {
 
-  it('should return a string "hi" when a user hits the "/" endpoint', async(done) => {
+  it('should return a string "hi" when a user hits the "/" endpoint', () => {
     return request(app)
       .get('/')
       .then(res => {
+        console.log(res.text);
         expect(res.text).toEqual('hi');
-        done();
       });
   });
   
-  it('should return <h1>red</h1> when a user hits the "red" endpoint', async(done) => {
+  it('should return <h1>red</h1> when a user hits the "red" endpoint', () => {
     return request(app)
       .get('/red')
       .then(res => {
-        expect(res.text).toEqual('<h1>red</h1>');
-        done();
+        expect(res.text).toEqual('<h1>Red</h1>');
       });
   });
   
-  it('should return <h1>blue</h1> when a user hits the "blue" endpoint', async(done) => {
+  it('should return <h1>blue</h1> when a user hits the "blue" endpoint', () => {
     return request(app)
       .get('/blue')
       .then(res => {
-        expect(res.text).toEqual('<h1>blue</h1>');
-        done();
+        expect(res.text).toEqual('<h1>Blue</h1>');
       });
   });
   
-  it('should return <h1>green</h1> when a user hits the "green" endpoint', async(done) => {
+  it('should return <h1>green</h1> when a user hits the "Green" endpoint', () => {
     return request(app)
       .get('/green')
       .then(res => {
-        expect(res.text).toEqual('<h1>green</h1>');
-        done();
+        expect(res.text).toEqual('<h1>Green</h1>');
       });
   });
   
-  it('should return the body when a user hits the "echo" endpoint', async(done) => {
+  it('should return the body when a user hits the "echo" endpoint', () => {
     return request(app)
       .post('/echo')
       .send({
-        number: 1
+        number: 4
       })
       .then(res => {
-        expect(res.text).toEqual('{"number":1}');
-        done();
+        expect(res.text).toEqual('{"number":4}');
       });
   });
 });
